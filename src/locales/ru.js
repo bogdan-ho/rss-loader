47ca4b3719ad89bfd4570c7fb586d5256e4b2fb3
Bogdan <homoretsky@gmail.com>
2022-10-13 09:33:00 +0300

    Add locales for i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    validation: {
+      success: 'RSS успешно загружен',
+      errors: {
+        rssExistAlready: 'RSS уже существует',
+        shouldNotBeEmpty: 'Не должно быть пустым',
+        rssShouldBeValidUrl: 'Ссылка должна быть валидным URL',
+      },
+    },
+    loading: {},
+    content: {},
+  },
+};
+
+// Ресурс не содержит валидный RSS, Просмотр, Ошибка сети.
