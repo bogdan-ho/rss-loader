06cddffcf02d80f4dfbfc080d7fce35f4ea36f01
Bogdan <homoretsky@gmail.com>
2022-10-17 13:19:00 +0300

    Add renderProcessStatus function

--- src/view.js
+++ src/view.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-console */
 import onChange from 'on-change';
 
-const urlInput = document.querySelector('#url-input');
-const feedbackEl = document.querySelector('.feedback');
+const renderValidationSuccess = (elements) => {
+  const { urlInput, feedbackEl } = elements;
 
-const renderValidationSuccess = () => {
   urlInput.classList.remove('is-invalid');
   urlInput.value = '';
   urlInput.focus();
@@ -14,7 +13,9 @@ const renderValidationSuccess = () => {
   feedbackEl.textContent = 'RSS успешно загружен';
 };
 
-const renderValidationFail = (validationErrors) => {
+const renderValidationFail = (validationErrors, elements) => {
+  const { urlInput, feedbackEl } = elements;
+
   urlInput.classList.add('is-invalid');
 
   feedbackEl.classList.remove('text-success');
@@ -22,24 +23,54 @@ const renderValidationFail = (validationErrors) => {
   feedbackEl.textContent = `${validationErrors}`;
 };
 
-export default (state) => onChange(state, (path, value, previousValue) => {
+const renderFormLoading = (elements) => {
+  const { rssForm, formButton } = elements;
+  rssForm.reset();
+  rssForm.focus();
+  formButton.disabled = true;
+};
+
+const renderProcessStatus = (value, elements) => {
+  switch (value) {
+    case ('loading'):
+      renderFormLoading(elements);
+      break;
+    case ('success'):
+      break;
+    case ('fail'):
+      break;
+    default:
+      throw new Error(`Wrong value: ${value}`);
+  }
+};
+
+export default (state, elements) => onChange(state, (path, value) => {
   console.log(`path is ${path}`);
 
   console.log(`value is ${JSON.stringify(value)}`);
   console.log(`value.at(-1) is ${JSON.stringify(value.at(-1))}`);
-  const currentFeed = value.at(-1);
-  const { urlValid, validationErrors } = currentFeed;
 
-  console.log(`urlValid is ${urlValid}`);
-  console.log(`validationErrors is ${JSON.stringify(validationErrors)}`);
-
-  console.log(`previousValue is ${JSON.stringify(previousValue)}`);
-
-  if (urlValid) {
-    console.log(`urlValid is ${urlValid} let's change`);
-    renderValidationSuccess();
-  } else {
-    console.log(`urlValid is ${urlValid} let's change`);
-    renderValidationFail(validationErrors);
+  switch (path) {
+    case ('form.process'):
+      renderProcessStatus(value, elements);
+      break;
+    default:
+      throw new Error(path);
   }
+
+  // const currentFeed = value.at(-1);
+  // const { urlValid, validationErrors } = currentFeed;
+
+  // console.log(`urlValid is ${urlValid}`);
+  // console.log(`validationErrors is ${JSON.stringify(validationErrors)}`);
+
+  // console.log(`previousValue is ${JSON.stringify(previousValue)}`);
+
+  // if (urlValid) {
+  //   console.log(`urlValid is ${urlValid} let's change`);
+  //   renderValidationSuccess(elements);
+  // } else {
+  //   console.log(`urlValid is ${urlValid} let's change`);
+  //   renderValidationFail(validationErrors, elements);
+  // }
 });
