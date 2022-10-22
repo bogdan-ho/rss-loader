00a11365783142bbb7bb2e7dc908aa877374e9e5
Bogdan <homoretsky@gmail.com>
2022-10-22 10:22:00 +0300

    Update view.js, add posts and feeds renders

--- src/view.js
+++ src/view.js
@@ -10,9 +10,106 @@ const renderErrors = (value, elements, i18n) => {
     case ('errors.rssAlreadyExist'):
       feedbackEl.textContent = i18n.t(value);
       break;
+    case ('errors.rssNotValid'):
+      feedbackEl.textContent = i18n.t(value);
+      break;
+    case ('AxiosError'):
+      feedbackEl.textContent = i18n.t('errors.networkError');
+      break;
     default:
       break;
   }
+};
+
+const renderFeeds = (value, elements, i18n) => {
+  const { feedsEl } = elements;
+
+  const cardEl = document.createElement('div');
+  cardEl.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const header = document.createElement('h2');
+  header.classList.add('card-title', 'h4');
+  header.textContent = i18n.t('feeds');
+
+  const listGroupEls = document.createElement('ul');
+  listGroupEls.classList.add('list-group', 'border-0', 'rounded-0');
+
+  value.forEach((feed) => {
+    const listGroupItem = document.createElement('li');
+    listGroupItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+    const feedHeader = document.createElement('h3');
+    feedHeader.classList.add('h6', 'm-0');
+    feedHeader.textContent = feed.title;
+
+    const feedDescription = document.createElement('p');
+    feedDescription.classList.add('m-0', 'small', 'text-black-50');
+    feedDescription.textContent = feed.description;
+
+    listGroupItem.append(feedHeader);
+    listGroupItem.append(feedDescription);
+
+    listGroupEls.append(listGroupItem);
+  });
+
+  cardBody.append(header);
+  cardEl.append(cardBody);
+  cardEl.append(listGroupEls);
+  feedsEl.innerHTML = '';
+  feedsEl.append(cardEl);
+};
+
+const renderPosts = (value, elements, i18n) => {
+  const { postsEl } = elements;
+
+  const cardEl = document.createElement('div');
+  cardEl.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const header = document.createElement('h2');
+  header.classList.add('card-title', 'h4');
+  header.textContent = i18n.t('feeds');
+
+  const listGroupEls = document.createElement('ul');
+  listGroupEls.classList.add('list-group', 'border-0', 'rounded-0');
+
+  value.forEach((post) => {
+    const listGroupItem = document.createElement('li');
+    listGroupItem.classList.add('list-group-item', 'border-0', 'border-end-0', 'd-flex', 'justify-content-between', 'align-items-start');
+
+    const postLink = document.createElement('a');
+    postLink.classList.add('fw-bold');
+    postLink.setAttribute('href', post.link);
+    postLink.setAttribute('target', '_blank');
+    postLink.setAttribute('rel', 'noopener noreferrer');
+    postLink.setAttribute('data-id', post.postId);
+    postLink.textContent = post.title;
+    // console.log(`post.title is ${post.title}`);
+    // console.log(`post is ${JSON.stringify(post)}`);
+
+    const postButton = document.createElement('button');
+    postButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    postButton.setAttribute('data-bs-toggle', 'modal');
+    postButton.setAttribute('data-bs-target', '#modal');
+    postButton.setAttribute('data-id', post.postId);
+    postButton.textContent = i18n.t('inspect');
+
+    listGroupItem.append(postLink);
+    listGroupItem.append(postButton);
+
+    listGroupEls.append(listGroupItem);
+  });
+
+  cardBody.append(header);
+  cardEl.append(cardBody);
+  cardEl.append(listGroupEls);
+  postsEl.innerHTML = '';
+  postsEl.append(cardEl);
 };
 
 const renderSuccessStatus = (value, elements, i18n) => {
@@ -23,6 +120,7 @@ const renderSuccessStatus = (value, elements, i18n) => {
   urlInput.value = '';
   urlInput.focus();
 
+  feedbackEl.classList.remove('text-muted');
   feedbackEl.classList.remove('text-danger');
   feedbackEl.classList.add('text-success');
   feedbackEl.textContent = i18n.t(value);
@@ -33,22 +131,25 @@ const renderFailStatus = (elements) => {
   formButton.disabled = false;
   urlInput.classList.add('is-invalid');
 
+  feedbackEl.classList.remove('text-muted');
   feedbackEl.classList.remove('text-success');
   feedbackEl.classList.add('text-danger');
 };
 
-const renderFormLoading = (elements) => {
-  const { rssForm, formButton } = elements;
+const renderFormLoading = (value, elements, i18n) => {
+  const { rssForm, formButton, feedbackEl } = elements;
 
   rssForm.focus();
   console.log(`formButton.disabled is ${formButton.disabled}`);
   formButton.disabled = true;
+  feedbackEl.classList.add('text-muted');
+  feedbackEl.textContent = i18n.t(value);
 };
 
 const renderProcessStatus = (value, elements, i18n) => {
   switch (value) {
     case ('loading'):
-      renderFormLoading(elements);
+      renderFormLoading(value, elements, i18n);
       break;
     case ('success'):
       renderSuccessStatus(value, elements, i18n);
@@ -70,12 +171,20 @@ export default (state, elements, i18n) => onChange(state, (path, value) => {
       renderProcessStatus(value, elements, i18n);
       break;
     case ('form.errors'):
-      console.log(`case ('form.errors') is ${path}`);
+      console.log(`case is ${path}`);
       renderErrors(value, elements, i18n);
       break;
     case ('links'):
-      console.log(`case ('links') is ${path}`);
+      // console.log(`case ('links') is ${path}`);
       // renderErrors(value, elements, i18n);
+      break;
+    case ('feeds'):
+      console.log(`case is ${path}`);
+      renderFeeds(value, elements, i18n);
+      break;
+    case ('posts'):
+      console.log(`case is ${path}`);
+      renderPosts(value, elements, i18n);
       break;
     default:
       throw new Error(path);
