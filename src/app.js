93a190ab0d5294be66e292a9f4a85c01e54370c1
Bogdan <homoretsky@gmail.com>
2022-10-25 17:22:00 +0300

    Delete console.logs

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import parser from './parser';
 
 const app = () => {
   // Model
-
   // создание экземпляра i18next
   const i18n = i18next.createInstance();
   i18n.init({
@@ -60,8 +59,6 @@ const app = () => {
     const schema = yup.string().required()
       .url()
       .notOneOf(feeds);
-    // console.log(`state.links is ${JSON.stringify(state.links)}`);
-    // console.log(`feeds is ${JSON.stringify(feeds)}`);
 
     return schema.validate(url);
   };
@@ -85,22 +82,15 @@ const app = () => {
 
     const formData = new FormData(e.target);
     const rssUrl = formData.get('url');
-    // console.log(`rssUrl is ${rssUrl}`);
 
     validate(watchedState.links, rssUrl)
       .then((validUrl) => {
-        // console.log(`validUrl is ${validUrl}`);
-        // console.log(`then state is ${JSON.stringify(watchedState)}`);
-
         watchedState.links.push(validUrl);
         const proxiedUrl = createProxiedUrl(validUrl);
-        // console.log(`proxiedUrl is ${proxiedUrl}`);
         axios.get(proxiedUrl)
           .then((content) => {
             watchedState.form.process = 'success';
             watchedState.links.push(validUrl);
-
-            // console.log(`content is ${JSON.stringify(content)}`);
             try {
               const { feed, posts } = parser(content);
               const feedId = _.uniqueId();
@@ -114,30 +104,22 @@ const app = () => {
               watchedState.form.process = 'fail';
               watchedState.form.errors = err.message;
             }
-            // console.log(`feed is ${feed}`);
-            // console.log(`state after adding feed is ${JSON.stringify(watchedState)}`);
           }).catch((err) => {
-            // console.log(`catch after axios is  ${JSON.stringify(err)}`);
-            // console.log(`err.message is  ${JSON.stringify(err.message)}`);
             // handle axios errors
             watchedState.form.process = 'fail';
             watchedState.form.errors = err.name;
           });
       })
       .catch((err) => {
-        watchedState.form.process = 'fail';
-
         // handle validation errors
+        watchedState.form.process = 'fail';
         watchedState.form.errors = err.message;
-
-        // console.log(`catch state is ${JSON.stringify(watchedState)}`);
       });
   });
 
   postsEl.addEventListener('click', (e) => {
     const postLink = e.target.href ?? e.target.previousElementSibling.href;
     const currentPost = watchedState.posts.find((post) => post.link === postLink);
-    // console.log(`currentPost is ${JSON.stringify(currentPost)}`);
     if (!currentPost) return;
 
     watchedState.currentPost = currentPost;
@@ -154,11 +136,8 @@ const app = () => {
       const getNewPosts = axios.get(proxiedUrl)
         .then((content) => {
           const oldPosts = posts.filter((post) => post.feedId === feedId);
-          // console.log(`oldPosts is ${JSON.stringify(oldPosts)}`);
-
           const currentPosts = parser(content).posts;
           const formattedPosts = currentPosts.map((post) => ({ ...post, validUrl, feedId }));
-          // console.log(`formattedPosts is ${JSON.stringify(formattedPosts)}`);
 
           const oldPostsNoPostId = oldPosts.map((post) => {
             const { title, description, link } = post;
@@ -170,8 +149,6 @@ const app = () => {
           });
 
           const postsDiff = _.differenceWith(currentPostsNoPostId, oldPostsNoPostId, _.isEqual);
-          // console.log(`postsDiff is ${JSON.stringify(postsDiff)}`);
-
           if (postsDiff.length > 0) {
             postsDiff.forEach((post) => {
               const postId = _.uniqueId();
